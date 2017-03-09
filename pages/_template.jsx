09519fefeca4e20792ab9bec566f646d63bce0ac
rorz <rory.mcmeekin@me.com>
2017-03-09 17:24:03 +0000

    first commit with retro style

---
+++ pages/_template.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import { Container } from 'react-responsive-grid'
+import { Link } from 'react-router'
+import { prefixLink } from 'gatsby-helpers'
+import Headroom from 'react-headroom'
+import '../css/markdown-styles'
+import '../css/pagestyles.scss'
+
+import { rhythm } from '../utils/typography'
+
+module.exports = React.createClass({
+  propTypes () {
+    return {
+      children: React.PropTypes.any,
+    }
+  },
+  render () {
+    return (
+      <div>
+        <Headroom
+          wrapperStyle={{
+            marginBottom: rhythm(1),
+          }}
+          style={{
+            background: '#222',
+          }}
+        >
+          <Container
+            style={{
+              maxWidth: 960,
+              paddingTop: 0,
+              padding: `${rhythm(1)} ${rhythm(3/4)}`,
+            }}
+          >
+            <Link
+              to={prefixLink('/')}
+              className="retro-title"
+            >
+              RORY<span style={{marginLeft: '1rem', fontSize: '.6rem'}}> &nbsp;&nbsp;&nbsp;&nbsp;これはイースターエッグです</span>
+                </Link>
+          </Container>
+        </Headroom>
+        <Container
+          style={{
+            maxWidth: 960,
+            padding: `${rhythm(1)} ${rhythm(3/4)}`,
+            paddingTop: 0,
+          }}
+        >
+          {this.props.children}
+        </Container>
+      </div>
+    )
+  },
+})
